0381aed536fbff76e20485f64c9cfe1e5b5e4eff
muktadir1280 <muktadir1280@gmail.com>
2024-09-20 20:47:06 +0600

    done the form

---
+++ form without function/js/features.js
@@ -0,0 +1,11 @@
+document.getElementById('cash-out')
+    .addEventListener('click', function() {
+        document.getElementById('cash-out-form').classList.remove('hidden');
+        document.getElementById('add-money-form').classList.add('hidden');
+    })
+
+document.getElementById('add-money')
+    .addEventListener('click', function() {
+        document.getElementById('cash-out-form').classList.add('hidden');
+        document.getElementById('add-money-form').classList.remove('hidden');
+    })
